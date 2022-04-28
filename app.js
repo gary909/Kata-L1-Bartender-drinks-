633ec0d8fdf080f48d1180397a5f977329816c5a
gary <gary.white@lodgit.com>
2022-04-28 13:25:00 +0200

    code

---
+++ app.js
@@ -0,0 +1,8 @@
+function getDrinkByProfession(param) {
+
+}
+
+console.log(getDrinkByProfession("jabrOni")); // "Patron Tequila"
+console.log(getDrinkByProfession("scHOOl counselor")); // "Anything with Alcohol"
+console.log(getDrinkByProfession("prOgramMer")); // "Hipster Craft Beer"
+console.log(getDrinkByProfession("bike ganG member")); // "Moonshine"
